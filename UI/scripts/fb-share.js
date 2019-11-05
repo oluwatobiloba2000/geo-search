1bde68c50c4cc2ade15f23be79d157ae8b522532
Anani oluwatobiloba <ananioluwatobiloba2000@gmail.com>
2019-11-05 15:29:54 +0100

    added fb share button and modified place api.js

--- UI/scripts/fb-share.js
+++ UI/scripts/fb-share.js
@@ -4,11 +4,11 @@
 //       js = d.createElement(s); js.id = id;
 //       js.src =  "//connect.facebook.net/en_US/sdk.js#xfbml=1&appId=1732816256851456&version=v2.0";
 //       fjs.parentNode.insertBefore(js, fjs);
-//     }(document, 'script', 'facebook-jssdk'));
-(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1";
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
+// //     }(document, 'script', 'facebook-jssdk'));
+// (function(d, s, id) {
+//   var js, fjs = d.getElementsByTagName(s)[0];
+//   if (d.getElementById(id)) return;
+//   js = d.createElement(s); js.id = id;
+//   js.src = "//connect.facebook.net/en_US/all.js#xfbml=1";
+//   fjs.parentNode.insertBefore(js, fjs);
+// }(document, 'script', 'facebook-jssdk'));
--- UI/scripts/placeApi.js
+++ UI/scripts/placeApi.js
@@ -3,6 +3,7 @@ const inputforMap = document.getElementById('search__bar')
 const button = document.getElementById('search__btn');
 const autocomplete = new google.maps.places.Autocomplete(inputforMap);
 
+
 button.addEventListener('click', () => {
     getQuery();
 }, )
@@ -39,6 +40,7 @@ function ActivityLogLoader(open, close) {
 
 function ErrorStatusLog(data) {
     ModalActivityLog("open");
+    CheckIfLogsAreGreaterThan7()
     let modal = document.querySelector('.alert__container')
     const errorDiv = document.createElement('div');
     errorDiv.innerHTML = ` <p>${data} <i class="fas fa-exclamation-triangle"></i></p>`
@@ -46,24 +48,16 @@ function ErrorStatusLog(data) {
     return modal.append(errorDiv);
 }
 
-function ClearActivityLogs() {
+function CheckIfLogsAreGreaterThan7(){
     let modal = document.querySelector('.alert__container');
-    if (modal.childNodes.length > 0) {
-        let i;
-        for (i = 0; i < modal.childNodes.length; i++) {
-            modal.removeChild(modal.childNodes[i]);
-        }
-        setTimeout(() => {
-            for (i = 0; i < modal.childNodes.length; i++) {
-                modal.removeChild(modal.childNodes[i]);
-            }
-        }, 900)
+    if (modal.childNodes.length === 7) {
+        modal.removeChild(modal.childNodes[0]);
     }
 }
 
-
 function OkayStatusLog(data) {
     ModalActivityLog("open");
+    CheckIfLogsAreGreaterThan7()
     let modal = document.querySelector('.alert__container')
     const okayDiv = document.createElement('div');
     okayDiv.innerHTML = ` <p>${data} <i class="fas fa-check"></i></p>`
@@ -73,6 +67,7 @@ function OkayStatusLog(data) {
 
 function WaitingStatusLog(data) {
     ModalActivityLog("open");
+    CheckIfLogsAreGreaterThan7()
     let modal = document.querySelector('.alert__container')
     const waitingDiv = document.createElement('div');
     waitingDiv.innerHTML = ` <p>${data} <i class="far fa-dot-circle"></i></p>`
@@ -93,24 +88,26 @@ function searchButtonLoader(add__loader, remove__loader) {
         button.innerHTML = `Search`;
         button.classList.remove('search__btn__loading');
     }
-
+    
 }
+
+document.querySelector('.close__loader__btn').addEventListener('click', () => {
+    const placesearchQueryFromLocalstorage = localStorage.getItem('place-search-query');
+    const placesearchQuery = JSON.parse(placesearchQueryFromLocalstorage);
+ if (placesearchQuery) {
+        return ModalActivityLog('', 'close')
+     }else if(placesearchQuery == null || placesearchQuery == undefined) {
+                console.log(placesearchQuery);
+                return ErrorStatusLog('No recent search has been made');
+            }
+})
 
 function getQuery() {
     let userSearchQueryValue = input.value.trim()
     if (userSearchQueryValue == '') {
         ActivityLogLoader('', 'close')
         document.querySelector('.close__loader__btn').style.display = 'block';
-        document.querySelector('.close__loader__btn').addEventListener('click', () => {
-            const placesearchQueryFromLocalstorage = localStorage.getItem('place-search-query');
-            const placesearchQuery = JSON.parse(placesearchQueryFromLocalstorage);
-            if (placesearchQuery) {
-                return ModalActivityLog('', 'close')
-            } else {
-                return ErrorStatusLog('No recent search has been made');
-            }
-        })
-        return ErrorStatusLog("search input cannot be empty")
+        return ErrorStatusLog("search input cannot be empty");
     }
     localStorage.setItem('user-search-query', JSON.stringify(userSearchQueryValue))
     searchButtonLoader('add__loader');
@@ -138,7 +135,7 @@ function fetchData() {
             return OkayStatusLog(`Welcome to Geo search <br> To get started, type in an address eg.<strong>[cityname],[state],[country]</strong> and you will see the current weather
         forcast, map view of the location.
         <br> To search click on the search button or press 'ENTER' key on your keyboard <br />
-        Please dont forget to leave a like [Just click on the like button when you see it, Thanks! 🙂]
+        Please dont forget to leave a like [Just click on the like button when you see it, Thanks! <span class="smile__emoji">🙂</span>]
         <br /> This app was made by Anani Oluwatobiloba`);
         }
 
@@ -185,7 +182,8 @@ function fetchData() {
                     }, 2000)
                 } else {
                     LoadMap(position.lat, position.lng)
-                    getWeather(position.lat, position.lng)
+                    TODO:
+                    // getWeather(position.lat, position.lng)
                     localStorage.setItem('place-search-query', JSON.stringify(result.Response.View[0].Result[0].Location.Address.Label))
                     const placesearchQueryFromLocalstorage = localStorage.getItem('place-search-query');
                     const placesearchQuery = JSON.parse(placesearchQueryFromLocalstorage);
@@ -208,24 +206,38 @@ function fetchData() {
         });
 
         function LoadMap(lat, lng) {
-            var mapProp = {
-                center: new google.maps.LatLng(lat, lng),
-                zoom: 9,
-                mapTypeId: google.maps.MapTypeId.ROADMAP
-            };
-            var map = new google.maps.Map(document.getElementById("map"), mapProp);
-            var markerCenter = new google.maps.LatLng(lat, lng);
-            // console.log(lat, lng)
+            let mapContainer = document.getElementById('map');
+            // var mapProp = {
+            //     center: new google.maps.LatLng(lat, lng),
+            //     zoom: 9,
+            //     mapTypeId: google.maps.MapTypeId.ROADMAP
+            // };
+            // var map = new google.maps.Map(document.getElementById("map"), mapProp);
+            // var markerCenter = new google.maps.LatLng(lat, lng);
+            // // console.log(lat, lng)
+            // https://www.mapquestapi.com/staticmap/v5/map?key=H1snoR7HAMGka2pLiA1cDnQiU2mVEc5x&locations=47.603229,-122.33028&size=900,500@2x
+            fetch(`https://www.mapquestapi.com/staticmap/v5/map?key=H1snoR7HAMGka2pLiA1cDnQiU2mVEc5x&locations=${lat},${lng}&size=1100,500@2x`)
+            .then(function(data){
+                OkayStatusLog('Map loaded sucessfully ..............');
+                mapContainer.innerHTML =`<img src="${data.url}" alt="map showing the place you searched">`
+
+                getWeather(lat, lng)
+            }).catch(function(e){
+                searchButtonLoader('', 'remove__loader');
+                ActivityLogLoader('', 'close')
+                ErrorStatusLog(e);
+               return console.log(e);
+            })
+
             if (lat == undefined || lng == undefined) {
                 searchButtonLoader('', 'remove__loader');
                 return ErrorStatusLog('Sorry!, No data for this location')
             }
-            var marker = new google.maps.Marker({
-                position: markerCenter,
-                animation: google.maps.Animation.BOUNCE
-            });
-            marker.setMap(map);
-            OkayStatusLog('Map loaded sucessfully ..............');
+            // var marker = new google.maps.Marker({
+            //     position: markerCenter,
+            //     animation: google.maps.Animation.BOUNCE
+            // });
+            // marker.setMap(map);
         }
         //settimeout close
     }, 2000)
@@ -247,7 +259,6 @@ function getWeather(lat, lon) {
             OkayStatusLog('Weather Data is Avaliable ..');
             setTimeout(() => {
                 ModalActivityLog('', 'close');
-                ClearActivityLogs()
             }, 2000)
             const celciusTemperature = Math.round(parseFloat(data.main.temp) - 273.15);
             const fahrenheit = Math.round(((parseFloat(data.main.temp) - 273.15) * 1.8) + 32);
@@ -285,7 +296,7 @@ function getWeather(lat, lon) {
 }
 
 function getPlacePhotoReference(placeName) {
-    let key = 'AIzaSyCfV-yKThJMx3FJcPrYzinBiaZtCIulIfA';
+    let key = 'AIzaSyAvQWut7PvB0bmndRW65xjWBQ7P06vO-cU';
     const proxyurl = "https://cors-anywhere.herokuapp.com/";
     const url = `https://maps.googleapis.com/maps/api/place/textsearch/json?query=${placeName}&key=${key}`; // site that doesn’t send Access-Control-*
     fetch(proxyurl + url)
@@ -296,7 +307,7 @@ function getPlacePhotoReference(placeName) {
 }
 
 function getPlacesImages(photoRefrence, placeName) {
-    let key = 'AIzaSyCfV-yKThJMx3FJcPrYzinBiaZtCIulIfA';
+    let key = 'AIzaSyAvQWut7PvB0bmndRW65xjWBQ7P06vO-cU';
     const proxyurl = "https://cors-anywhere.herokuapp.com/";
     // https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=CnRtAAAATLZNl354RwP_9UKbQ_5Psy40texXePv4oAlgP4qNEkdIrkyse7rPXYGd9D_Uj1rVsQdWT4oRz4QrYAJNpFX7rzqqMlZw2h2E2y5IKMUZ7ouD_SlcHxYq1yL4KbKUv3qtWgTK0A6QbGh87GB3sscrHRIQiG2RrmU_jF4tENr9wGS_YxoUSSDrYjWmrNfeEHSGSc3FyhNLlBU&key=YOUR_API_KEY
     fetch(`${proxyurl}https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=${photoRefrence}&key=${key}`)
